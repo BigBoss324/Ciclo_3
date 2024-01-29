8106e4d8bdd9e7280f8afb7a60cca86044612753
Alexis Taha <robertogomezalfa@gmail.com>
2024-01-29 12:55:30 -0300

    Actualización de código

--- model_prediction_bikerpro.py
+++ model_prediction_bikerpro.py
@@ -1,27 +1,21 @@
-import numpy as np
-import pandas as pd
+# lets import train test split for splitting the data
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV 
+from sklearn.preprocessing import  MinMaxScaler, OneHotEncoder
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import Ridge
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler, PolynomialFeatures,OrdinalEncoder,LabelBinarizer
-from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import SelectKBest, r_regression
+import numpy as np
 import warnings
+import pandas as pd
 import os
-import seaborn as sns
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsRegressor
 import pickle
-from sklearn.base import BaseEstimator, TransformerMixin
-from scipy import stats
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.feature_selection import SelectKBest, mutual_info_regression
-from sklearn.base import BaseEstimator, TransformerMixin
-from scipy import stats
-from sklearn.model_selection import RandomizedSearchCV
-from math import sin, cos, pi
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
 
 
 warnings.filterwarnings('ignore')
@@ -31,58 +25,41 @@ DATA_PATH = "C:/Users/Alexis Taha/Desktop/digital nao"
 FILE_BIKERPRO = 'SeoulBikeData.csv'
 bikerpro = pd.read_csv(os.path.join(DATA_PATH, FILE_BIKERPRO), encoding="ISO-8859-1")
 
-# Limpieza y preprocesamiento de datos
-clean_columns = [x.lower().replace("(°c)", '').replace("(%)", '').replace(" (m/s)", '').replace(" (10m)", '').replace(" (mj/m2)", '').replace("(mm)", '').replace(" (cm)", '').replace(" ", '_') for x in bikerpro.columns]
-bikerpro.columns = clean_columns
-bikerpro['date'] = pd.to_datetime(bikerpro['date'], format='%d/%m/%Y')
 
-# Transformaciones de variables
-bikerpro['hour'] = bikerpro['hour'].astype('category')
-bikerpro['weekday'] = bikerpro['date'].dt.weekday.astype('category')
-bikerpro['month'] = bikerpro['date'].dt.month.astype('category')
-bikerpro['is_weekend'] = bikerpro['weekday'].isin([5, 6]).astype(int)
-bikerpro['holiday'] = bikerpro['holiday'].map({'No Holiday': 0, 'Holiday': 1})
-bikerpro['functioning_day'] = bikerpro['functioning_day'].map({'No': 0, 'Yes': 1})
+bikerpro['Date'] = pd.to_datetime(bikerpro['Date'], format='%d/%m/%Y')
 
-# Definición de columnas para el modelado
-weather_cols = ['temperature', 'humidity', 'wind_speed', 'visibility', 'solar_radiation', 'rainfall', 'snowfall']  # Excluyendo 'dew_point_temperature'
-categorical_cols = ['seasons', 'hour', 'weekday']  # Añadiendo 'weekday'
-binary_cols = ['holiday', 'functioning_day', 'is_weekend']  # Añadiendo 'functional_day'
+bikerpro['Month'] = bikerpro['Date'].dt.month.astype('category')
+bikerpro['Day'] = bikerpro['Date'].dt.day_name()    
+bikerpro['Weekdays_or_weekend'] = bikerpro['Day'].apply(lambda x: 1 if x=='Saturday' or x=='Sunday' else 0)
 
-# Preprocesamiento y división de los datos
-X = bikerpro[weather_cols + categorical_cols + binary_cols]
-y = bikerpro['rented_bike_count']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.18, random_state=42)
+bikerpro.drop(['Date',"Day"],axis=1 ,inplace=True)
 
+df = bikerpro.copy()
 
+df.drop(['Humidity(%)','Visibility (10m)'],inplace=True,axis=1)
 
-class YeoJohnsonTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, columns=None):
-        self.columns = columns
-        self.lambdas_ = None
+df['Spring'] = np.where(df['Seasons'] == 'Spring', 1, 0)
+df['Summer'] = np.where(df['Seasons'] == 'Summer', 1, 0)
+df['Autumn'] = np.where(df['Seasons'] == 'Autumn', 1, 0)
+df['Winter'] = np.where(df['Seasons'] == 'Winter', 1, 0)
 
-    def fit(self, X, y=None):
-        self.lambdas_ = {}
-        for col in self.columns:
-            _, self.lambdas_[col] = stats.yeojohnson(X[col])
-        return self
+df.drop(columns=['Seasons'],axis=1,inplace=True)
+df['Holiday'] = df['Holiday'].map({'No Holiday':0, 'Holiday':1})
+df['Functioning Day'] = df['Functioning Day'].map({'Yes':1, 'No':0})
 
-    def transform(self, X, y=None):
-        X_trans = X.copy()
-        for col in self.columns:
-            X_trans[col] = stats.yeojohnson(X[col], lmbda=self.lambdas_[col])
-        return X_trans
+y = np.sqrt(df['Rented Bike Count'])
 
+X = df.drop('Rented Bike Count',axis=1)
 
-cols_to_transform = ['temperature', 'humidity', 'wind_speed', 'visibility','solar_radiation', 'rainfall', 'snowfall']
-  
+categorical_cols = X.select_dtypes(include='object').columns
+numerical_cols = X.select_dtypes(exclude='object').columns
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 
 # Definición de los pipelines
-numerical_pipe = Pipeline([
-    ('yeojohnson', YeoJohnsonTransformer(columns=cols_to_transform)),
-    ('standar_scaler', MinMaxScaler()),
-    ('select_best', SelectKBest(mutual_info_regression, k=4)),
-    ('polynomial', PolynomialFeatures(degree=2))
+numerical_pipe = Pipeline([  # Uso de PowerTransformer
+    ('min_max_scaler', MinMaxScaler())
 ])
 
 categorical_pipe = Pipeline([
@@ -90,121 +67,150 @@ categorical_pipe = Pipeline([
 ])
 
 pre_processor = ColumnTransformer([
-    ('numerical', numerical_pipe, weather_cols),
+    ('numerical', numerical_pipe, numerical_cols),
     ('categorical', categorical_pipe, categorical_cols),
 ], remainder='passthrough')
 
 # Pipeline para Random Forest
 pipe_rf = Pipeline([
-    ('transform', pre_processor),
+    ('transform', numerical_pipe),
     ('model', RandomForestRegressor(random_state=0))
 ])
 
 
 # Pipeline para Ridge
 pipe_ridge = Pipeline([
-    ('transform', pre_processor),
+    ('transform', numerical_pipe),
     ('model', Ridge(random_state=0))
 ])
 
 
 # Pipeline para K-NN
 pipe_knn = Pipeline([
-    ('transform', pre_processor),
+    ('transform', numerical_pipe),
     ('model', KNeighborsRegressor())
 ])
 
+
 # Parámetros para K-NN
 param_grid_knn = {
-    'model__n_neighbors': [20],
-    'model__weights': ['distance'],
-    'model__metric': ['manhattan']
+    'model__n_neighbors': [5, 10, 15, 20, 25],
+    'model__weights': ['uniform', 'distance'],
+    'model__metric': ['euclidean', 'manhattan', 'minkowski']
 }
-
 
 
 # Parámetros para Random Forest
 param_dist_rf = {
-    'model__n_estimators': [100],
-    'model__max_depth': [None],
-    'model__min_samples_split': [2],
-    'model__min_samples_leaf': [1],
-    'model__max_features': [None],
-    'model__min_impurity_decrease': [0.01],
-    'model__bootstrap' : [True]
+    'model__n_estimators': [50, 100, 150],
+    'model__max_depth': [5, 10, 15, None],
+    'model__min_samples_split': [2, 5, 10],
+    'model__min_samples_leaf': [1, 2, 4]
 }
 
 # Parámetros para Ridge
 param_grid_ridge = {
-    'model__alpha': [1]
+    'model__alpha': [0.1, 0.5, 1.0, 2.0, 5.0]
 }
 
+tscv = TimeSeriesSplit(n_splits=4, test_size=2)
 
-# Ajuste y evaluación de cada modelo
-cv_rf = random_search_rf = RandomizedSearchCV(
-    pipe_rf,
-    param_distributions=param_dist_rf,
-    n_iter=100,    # Número de iteraciones de búsqueda
-    cv=5,          # Validación cruzada de 5 pliegues
-    scoring='neg_mean_squared_error',  # Puedes cambiar esto según tu métrica objetivo
-    random_state=42,
-    n_jobs=-1      # Usa todos los núcleos disponibles
-).fit(X_train, y_train)
+cv_rf = GridSearchCV(
+    estimator=pipe_rf,
+    param_grid=param_dist_rf,  # Uso de 'param_grid'
+    cv=tscv,                   # TimeSeriesSplit como estrategia de validación cruzada
+    scoring='neg_mean_squared_error',
+    n_jobs=-1                  # Usa todos los núcleos disponibles
+)
 
-cv_ridge = RandomizedSearchCV(pipe_ridge, param_grid_ridge, n_jobs=-1, scoring='neg_mean_squared_error', cv=5).fit(X_train, y_train)
-cv_knn = RandomizedSearchCV(pipe_knn, param_grid_knn, n_jobs=-1, scoring='neg_mean_squared_error', cv=5).fit(X_train, y_train)
+cv_rf.fit(X_train, y_train)
 
+
+cv_ridge = GridSearchCV(
+estimator=pipe_ridge,
+param_grid=param_grid_ridge,
+cv=tscv, # TimeSeriesSplit como estrategia de validación cruzada
+scoring='neg_mean_squared_error',
+n_jobs=-1 # Usa todos los núcleos disponibles
+)
+
+cv_ridge.fit(X_train, y_train)
+
+
+cv_knn = GridSearchCV(
+estimator=pipe_knn,
+param_grid=param_grid_knn,
+cv=tscv, # TimeSeriesSplit como estrategia de validación cruzada
+scoring='neg_mean_squared_error',
+n_jobs=-1 # Usa todos los núcleos disponibles
+)
+
+cv_knn.fit(X_train, y_train)
+
+print("------------------------")
+
+# Mejores parámetros para Random Forest
+print("Mejores parámetros para Random Forest:")
+print(cv_rf.best_params_)
+
+# Mejores parámetros para Ridge
+print("\nMejores parámetros para Ridge:")
+print(cv_ridge.best_params_)
+
+# Mejores parámetros para K-NN
+print("\nMejores parámetros para K-NN:")
+print(cv_knn.best_params_)
+
+print("-------------------------")
+print(" ")
 
 # Evaluación de los modelos en el conjunto de prueba y de entrenamiento
 error_rf_train = mean_squared_error(y_train, cv_rf.predict(X_train), squared=False)
 error_ridge_train = mean_squared_error(y_train, cv_ridge.predict(X_train), squared=False)
 error_knn_train = mean_squared_error(y_train, cv_knn.predict(X_train), squared=False)
 
+print("-------------------------")
 # Imprimir resultados de RMSE para cada modelo en el conjunto de prueba
 print("Evaluación de Modelos en el Conjunto de Entrenamiento")
-print(f"Random Forest - RMSE: {error_rf_train.round(2)}")
-print(f"Ridge - RMSE: {error_ridge_train.round(2)}")
-print(f"K-NN - RMSE: {error_knn_train.round(2)}")
-
+print(f"Random Forest - RMSE: {error_rf_train}")
+print(f"Ridge - RMSE: {error_ridge_train}")
+print(f"K-NN - RMSE: {error_knn_train}")
+print("-------------------------")
+print(" ")
 
 # Evaluación de los modelos en el conjunto de prueba
 error_rf_test = mean_squared_error(y_test, cv_rf.predict(X_test), squared=False)
 error_ridge_test = mean_squared_error(y_test, cv_ridge.predict(X_test), squared=False)
 error_knn_test = mean_squared_error(y_test, cv_knn.predict(X_test), squared=False)
 
+print("-------------------------")
 # Imprimir resultados de RMSE para cada modelo en el conjunto de prueba
 print("Evaluación de Modelos en el Conjunto de Prueba")
-print(f"Random Forest - RMSE: {error_rf_test.round(2)}")
-print(f"Ridge - RMSE: {error_ridge_test.round(2)}")
-print(f"K-NN - RMSE: {error_knn_test.round(2)}")
-
+print(f"Random Forest - RMSE: {error_rf_test}")
+print(f"Ridge - RMSE: {error_ridge_test}")
+print(f"K-NN - RMSE: {error_knn_test}")
+print("-------------------------")
+print(" ")
 
 # Seleccionar el modelo con el menor error en el conjunto de prueba
 errors = {'Random Forest': error_rf_test, 'Ridge': error_ridge_test, 'K-NN': error_knn_test}
 best_model_name = min(errors, key=errors.get)
 best_model = {'Random Forest': cv_rf, 'Ridge': cv_ridge, 'K-NN': cv_knn}[best_model_name]
 
-# Asumiendo que best_model_rf es tu mejor modelo Random Forest
+# Hacer predicciones
 y_pred_train = best_model.predict(X_train)
 y_pred_test = best_model.predict(X_test)
 
 # Crear DataFrames para comparar los valores reales y predichos
-train_comparison = pd.DataFrame({'Real': y_train, 'Predicción': y_pred_train, 'Error': y_train - y_pred_train})
-test_comparison = pd.DataFrame({'Real': y_test, 'Predicción': y_pred_test, 'Error': y_test - y_pred_test})
+train_comparison = pd.DataFrame({'Real': y_train, 'Predicción': y_pred_train})
+train_comparison['Error'] = train_comparison['Real'] - train_comparison['Predicción']
 
-# Puedes agrupar los datos por una característica específica para ver si hay patrones
-error_by_hour = train_comparison.groupby(bikerpro['hour'])['Error'].mean()
-plt.figure(figsize=(10, 5))
-error_by_hour.plot(kind='bar')
-plt.xlabel('Hora del Día')
-plt.ylabel('Error Medio')
-plt.title('Error Medio por Hora del Día')
-plt.show()
-
+test_comparison = pd.DataFrame({'Real': y_test, 'Predicción': y_pred_test})
+test_comparison['Error'] = test_comparison['Real'] - test_comparison['Predicción']
 
 
 # Imprimir el modelo con el mejor rendimiento
-print(f"\nModelo con mejor rendimiento: {best_model_name} con RMSE: {errors[best_model_name].round(2)}")
+print(f"\nModelo con mejor rendimiento: {best_model_name} con RMSE: {errors[best_model_name]}")
 
 # Configuración de estilo de gráficos
 sns.set_style("whitegrid")
